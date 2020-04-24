dda848ee6c5dd0f96ed7882f4a6347603e44bd7a
James Coglan <jcoglan@gmail.com>
2020-04-24 13:09:56 +0100

    Implement print! and println! macros on top of VGA buffer
    Source: https://os.phil-opp.com/vga-text-mode/

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    print!("Hello again");
+    println!(", some numbers: {} {}", 42, 1.337);
 
     loop {}
 }
