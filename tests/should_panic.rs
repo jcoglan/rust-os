bc8878b1f4bf52f9d1b6259ca8faf99ba3e8f2a5
James Coglan <jcoglan@gmail.com>
2020-05-01 16:05:45 +0100

    Remove the test harness boilerplate from should_panic.rs
    Source: https://os.phil-opp.com/testing/

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,27 +1,15 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use rust_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
-}
-
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
 }
 
 #[panic_handler]
@@ -31,7 +19,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
 fn should_fail() {
     serial_print!("should_fail... ");
     assert_eq!(0, 1);
