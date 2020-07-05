f84f2b8aab36bbeb9317b3faaf36ac941eafe8ec
James Coglan <jcoglan@gmail.com>
2020-07-05 14:55:47 +0100

    Handle the periodic timer interrupt (PIT)
    We must wrap all printing operations in `interrupts::without_interrupts`
    to prevent interrupts being handled while we're holding a lock on the
    global WRITER mutex. Otherwise, an interrupt handler could deadlock the
    system by trying to print output.
    
    Source: https://os.phil-opp.com/hardware-interrupts/

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@ use core::panic::PanicInfo;
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
