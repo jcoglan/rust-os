fb8ee3abf08498632ca0a541992620fa8bebf846
James Coglan <jcoglan@gmail.com>
2020-07-06 12:13:13 +0100

    Translate virtual addresses to physical addresses
    This uses the "map physical memory" strategy for mapping the entire
    physical memory to a fixed offset in virtual memory. Translating the
    virtual address of the start of physical memory does not work yet
    because we don't support huge pages.
    
    Source: https://os.phil-opp.com/paging-implementation/

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,33 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_os::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+entry_point!(kernel_main);
 
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rust_os::memory::translate_addr;
+    use x86_64::VirtAddr;
+
+    println!("Hello World{}", "!");
     rust_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
