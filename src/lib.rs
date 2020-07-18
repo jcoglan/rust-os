b3296253e6ab7855c28d9b1a7313569bb3a6f4fb
James Coglan <jcoglan@gmail.com>
2020-07-18 11:47:46 +0100

    Add Waker support to the executor
    This replaces SimpleExecutor with Executor, a better implementation that
    actually makes use of wakers to re-schedule tasks, rather than polling
    all tasks in a loop. It means that if there are no tasks making process,
    we can run HLT and put the CPU to sleep.
    
    Source: https://os.phil-opp.com/async-await/

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 #![feature(const_fn)]
 #![feature(const_in_array_repeat_expressions)]
 #![feature(custom_test_frameworks)]
+#![feature(wake_trait)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_os::println;
-use rust_os::task::{keyboard, simple_executor::SimpleExecutor, Task};
+use rust_os::task::{executor::Executor, keyboard, Task};
 
 entry_point!(kernel_main);
 
@@ -51,7 +51,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         Rc::strong_count(&cloned_reference)
     );
 
-    let mut executor = SimpleExecutor::new();
+    let mut executor = Executor::new();
     executor.spawn(Task::new(example_task()));
     executor.spawn(Task::new(keyboard::print_keypresses()));
     executor.run();
